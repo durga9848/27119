f0df14ddf7059489680b452cabe0001c072e2549
PrasadGorela98 <prasad984853@gmail.com>
2024-03-16 23:28:56 +0000

    side navbar

--- app/app.js
+++ app/app.js
@@ -9,10 +9,12 @@ app.use(express.static("static"));
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
+app.set('view engine', 'pug');
+app.set('views', './app/views');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.render("index");
 });
 
 // Create a route for testing the db
