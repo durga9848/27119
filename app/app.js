1ba75790202350f589ecf4acab8c89bff5756427
RupaSravanth <tathinenirupasravanthi@gmail.com>
2024-04-28 00:58:18 +0100

    login,sessions and signup

--- app/app.js
+++ app/app.js
@@ -1,9 +1,26 @@
 // Import express.js
 const express = require("express");
-
+const { User } = require("./models/user");
 // Create express app
 var app = express();
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
+
+const session = require('express-session');
+const oneDay = 1000 * 60 * 60 * 24;
+const sessionMiddleware = session({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+});
+app.use(sessionMiddleware);
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Add static files location
 app.use(express.static("static"));
 
@@ -12,9 +29,95 @@ const db = require('./services/db');
 app.set('view engine', 'pug');
 app.set('views', './app/views');
 
-// Create a route for root - /
-app.get("/", function(req, res) {
-    res.render("index");
+// // Create a route for root - /
+// app.get("/", function(req, res) {
+//     res.render("index");
+// });
+
+
+app.get('/signup', function (req, res) {
+    res.render('signup');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+
+app.get("/", function (req, res) {
+    try {
+        if (req.session.uid) {
+            res.redirect('/home');
+        } else {
+            res.render('login');
+        }
+        res.end();
+    } catch (err) {
+        console.error("Error accessing root route:", err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
+// Check submitted email and password pair
+app.post('/authenticate', async function (req, res) {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).send('Email and password are required.');
+        }
+
+        var user = new User(email);
+        const uId = await user.getIdFromEmail();
+        if (!uId) {
+            return res.status(401).send('Invalid email');
+        }
+
+        const match = await user.authenticate(password);
+        if (!match) {
+            return res.status(401).send('Invalid password');
+        }
+
+        req.session.uid = uId;
+        req.session.loggedIn = true;
+        console.log(req.session.id);
+        res.redirect('/home');
+    } catch (err) {
+        console.error(`Error while authenticating user:`, err.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/set-password', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            // If a valid, existing user is found, set the password and redirect to the users single-student page
+            await user.setUserPassword(params.password);
+            console.log(req.session.id);
+            res.send('Password set successfully');
+        }
+        else {
+            // If no existing user is found, add a new one
+            newId = await user.addUser(params.email);
+            res.send('Perhaps a page where a new user sets a programme would be good here');
+        }
+    } catch (err) {
+        console.error(`Error while adding password `, err.message);
+    }
+});
+
+app.get('/logout', function (req, res) {
+    try {
+        req.session.destroy();
+        res.redirect('/login');
+    } catch (err) {
+        console.error("Error logging out:", err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 // Create a route for testing the db
