8bd88a227a3ebad1ed24372b8167a09a8bbc5eeb
HemaSri <hemasrivaddempudi@gmail.com>
2024-05-02 16:56:47 +0100

    company login and registration UI

---
+++ app/models/companies.js
@@ -0,0 +1,58 @@
+// Get the functions in the db.js file to use
+const db = require('../services/db');
+const bcrypt = require("bcryptjs");
+
+class Company {
+
+    // Id of the company
+    id;
+
+    // Email of the company
+    email;
+
+    constructor(email) {
+        this.email = email;
+    }
+    async getIdFromEmail() {
+        var sql = "SELECT id FROM Companys WHERE Companys.email = ?";
+        const result = await db.query(sql, [this.email]);
+        // TODO LOTS OF ERROR CHECKS HERE..
+        if (JSON.stringify(result) != '[]') {
+            this.id = result[0].id;
+            return this.id;
+        }
+        else {
+            return false;
+        }
+    }
+    async setUserPassword(password) {
+        const pw = await bcrypt.hash(password, 10);
+        var sql = "UPDATE Companys SET password = ? WHERE Companys.id = ?"
+        const result = await db.query(sql, [pw, this.id]);
+        return true;
+    }
+    async addUser(password) {
+        const pw = await bcrypt.hash(password, 10);
+        var sql = "INSERT INTO Companys (email, password) VALUES (? , ?)";
+        const result = await db.query(sql, [this.email, pw]);
+        console.log(result.insertId);
+        this.id = result.insertId;
+        return true;
+    }
+    async authenticate(submitted) {
+        // Get the stored, hashed password for the company
+        var sql = "SELECT password FROM Companys WHERE id = ?";
+        const result = await db.query(sql, [this.id]);
+        const match = await bcrypt.compare(submitted, result[0].password);
+        if (match == true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
+module.exports  = {
+    Company,
+}
